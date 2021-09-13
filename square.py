4d7cc899dff8c4ea6dc60d39d0847c7015f0de5a
candiepih <mutheeal.am@gmail.com>
2021-09-13 16:41:35 +0300

    formatted file to pep8 linting style

--- square.py
+++ square.py
@@ -1,12 +1,24 @@
 #!/usr/bin/python3
+"""Class Square
+Defines a squaare geometry
+"""
 
-class square():
-    
+
+class Square:
+    """Defines square geometry
+    Attributes:
+        width (int): the width of the square
+        height (int): the height of the square
+    """
     width = 0
     height = 0
 
-    
     def __init__(self, *args, **kwargs):
+        """Initializes the Square class
+        Args:
+            args: arguments passed on instance
+            kwargs: keyword arguments passes on instance
+        """
         for key, value in kwargs.items():
             setattr(self, key, value)
 
@@ -14,15 +26,18 @@ class square():
         """ Area of the square """
         return self.width * self.width
 
-    def PermiterOfMySquare(self):
+    def perimeter_of_my_square(self):
+        """ Perimeter of the square """
         return (self.width * 2) + (self.height * 2)
 
     def __str__(self):
+        """ String representation of the square"""
         return "{}/{}".format(self.width, self.height)
+
 
 if __name__ == "__main__":
 
-    s = square(width=12, height=9)
+    s = Square(width=12, height=9)
     print(s)
     print(s.area_of_my_square())
-    print(s.PermiterOfMySquare())
+    print(s.perimeter_of_my_square())
