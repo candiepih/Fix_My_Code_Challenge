e7c4ce49e4be7f233fb24fdb26b5fe55390feefc
Candiepih <44834632+candiepih@users.noreply.github.com>
2021-09-21 12:23:35 +0300

    Update square.py

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -24,11 +24,11 @@ class Square:
 
     def area_of_my_square(self):
         """ Area of the square """
-        return self.width * self.width
+        return self.width * self.height
 
     def perimeter_of_my_square(self):
         """ Perimeter of the square """
-        return (self.width * 2) + (self.width * 2)
+        return (self.width * 2) + (self.height * 2)
 
     def __str__(self):
         """ String representation of the square"""
